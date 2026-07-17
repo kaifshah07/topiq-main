e1fbbad6c466714d8e8084854969bc585a09d86e
Kaif Shah <shahkaif004@gmail.com>
2026-07-17 18:47:10 +0530

    vercel link added

--- Topiq backend/src/app.ts
+++ Topiq backend/src/app.ts
@@ -10,7 +10,15 @@ const app = express();
 
 app.use(helmet());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://topiq-main.vercel.app/", 
+    ],
+    credentials: true,
+  })
+);
 
 app.use(compression());
 
